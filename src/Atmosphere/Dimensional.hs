dc4729a672446a22dddd4e6b268102f7b623c106
Douglas McClean <douglas.mcclean@gmail.com>
2016-08-29 01:29:26 -0400

    Added calculation of CIPM-2007 equation for density of moist air.

--- src/Atmosphere/Dimensional.hs
+++ src/Atmosphere/Dimensional.hs
@@ -11,6 +11,7 @@ module Atmosphere.Dimensional
        , altitudeFromPressure
        , vaporPressureOfWaterAtSaturation
        , relativeHumidityFromTemperatureAndDewpoint
+       , densityOfAir
        ) where
 
 import qualified Atmosphere as A
@@ -61,3 +62,28 @@ enhancementFactor p t = alpha + beta * p + gamma * t^pos2
 
 relativeHumidityFromTemperatureAndDewpoint :: (Floating a) => Pressure a -> ThermodynamicTemperature a -> ThermodynamicTemperature a -> Dimensionless a
 relativeHumidityFromTemperatureAndDewpoint p t td = (enhancementFactor p td * vaporPressureOfWaterAtSaturation td) / (enhancementFactor p t * vaporPressureOfWaterAtSaturation t)
+
+compressibilityFactor :: (Fractional a) => Pressure a -> ThermodynamicTemperature a -> Dimensionless a -> Dimensionless a
+compressibilityFactor p t xv = _1 - ((p / t) * (a0 + a1 * t + a2 * t^pos2 + (b0 + b1 * t) * xv + (c0 + c1 * t) * xv^pos2)) + ((p^pos2 / t^pos2) * (d + e * xv^pos2))
+  where
+    a0 = 1.58123e-6 *~ (kelvin * pascal^neg1)
+    a1 = -2.9331e-8 *~ (pascal^neg1)
+    a2 = 1.1043e-10 *~ (kelvin^neg1 * pascal^neg1)
+    b0 = 5.707e-6 *~ (kelvin * pascal^neg1)
+    b1 = -2.051e-8 *~ (pascal^neg1)
+    c0 = 1.9898e-4 *~ (kelvin * pascal^neg1)
+    c1 = -2.376e-6 *~ (pascal^neg1)
+    d = 1.83e-11 *~ (kelvin^pos2 * pascal^neg2)
+    e = -0.765e-8 *~ (kelvin^pos2 * pascal^neg2)
+
+moleFractionOfWaterVapor :: (Floating a) => Pressure a -> ThermodynamicTemperature a -> Dimensionless a -> Dimensionless a
+moleFractionOfWaterVapor p t h = h * enhancementFactor p t * vaporPressureOfWaterAtSaturation t / p
+
+densityOfAir :: (Floating a) => Pressure a -> ThermodynamicTemperature a -> Dimensionless a -> Density a
+densityOfAir p t h = ((p * ma) / (z * r * t)) * (_1 - xv * (_1 - (mv / ma)))
+  where
+    xv = moleFractionOfWaterVapor p t h
+    r = 8.314472 *~ (joule * mole^neg1 * kelvin^neg1)
+    ma = 28.96546e-3 *~ (kilo gram / mole)
+    mv = 18.01528e-3 *~ (kilo gram / mole)
+    z = compressibilityFactor p t xv
